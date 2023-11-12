017952d6dcdf662ff204c27634795a6fb38530f1
Jakebrady900 <128072499+Jakebrady900@users.noreply.github.com>
2023-11-12 19:55:19 +0000

    Initiation

--- src/main/java/com/roomwise/RoomWiseApplication.java
+++ src/main/java/com/roomwise/RoomWiseApplication.java
@@ -2,12 +2,18 @@ package com.roomwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class RoomWiseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoomWiseApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String WelcomeMessage() {
+        return "Welcome to Room-Wise!";
     }
 
 }
