f38f3e849de86856630b2044873d6624e62da980
Jakebrady900 <128072499+Jakebrady900@users.noreply.github.com>
2023-11-12 19:53:11 +0000

    Initial commit

---
+++ src/main/java/com/roomwise/RoomWiseApplication.java
@@ -0,0 +1,13 @@
+package com.roomwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomWiseApplication.class, args);
+    }
+
+}
