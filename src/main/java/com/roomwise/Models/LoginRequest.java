2044e82c33b7d8a319c0f7949af87b0a81b7ede4
Jakebrady900 <128072499+Jakebrady900@users.noreply.github.com>
2023-12-03 19:08:30 +0000

    JWT

---
+++ src/main/java/com/roomwise/Models/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.roomwise.Models;
+
+public record LoginRequest(String username, String password) {
+
+}
--- src/main/java/com/roomwise/RoomWiseApplication.java
+++ src/main/java/com/roomwise/RoomWiseApplication.java
@@ -2,11 +2,14 @@ package com.roomwise;
 
 import com.roomwise.Models.Room;
 import com.roomwise.Room.Factories.*;
+import com.roomwise.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class RoomWiseApplication {
 
