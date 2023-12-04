876c83ab88a4dbe16299fa98bdc7df9ffb557474
Ashutoshul <150622789+Ashutoshul@users.noreply.github.com>
2023-12-04 15:53:31 +0000

    resDao

--- src/main/java/com/roomwise/Repositories/ReservationDAO.java
+++ src/main/java/com/roomwise/Repositories/ReservationDAO.java
@@ -1,11 +1,42 @@
 package com.roomwise.Repositories;
 
 import com.roomwise.Models.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservationDAO extends JpaRepository<Reservation, Long> {
-  
+public class ReservationDAO {
 
+    // List<Reservation> findByCustomer(Customer customer);
+
+    List<Reservation> ReservationDB = new ArrayList<>();
+
+    public void save(Reservation room) {
+        ReservationDB.add(room);
+    }
+
+    public List<Reservation> getReservations() {
+        return ReservationDB;
+    }
+
+    public Reservation findById(Integer Id) {
+        for (Reservation reservation : ReservationDB) {
+            if (reservation.getReservationId() == Id) {
+                return reservation;
+            }
+        }
+        return null;
+    }
+
+    public String deleteReservationById(Integer Id) {
+        if (findById(Id) != null) {
+            ReservationDB.remove(Id);
+            return "Reservation Canceled";
+        } else {
+            return "No Such Reservation";
+        }
+    }
 }
