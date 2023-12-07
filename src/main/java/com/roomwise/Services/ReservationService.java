02fbcd907f62f9eacc6169057eae22da6a9ea026
Jakebrady900 <128072499+Jakebrady900@users.noreply.github.com>
2023-12-07 13:38:22 +0000

    Calculating Charge

--- src/main/java/com/roomwise/Services/ReservationService.java
+++ src/main/java/com/roomwise/Services/ReservationService.java
@@ -2,10 +2,12 @@ package com.roomwise.Services;
 
 
 import com.roomwise.Models.Reservation;
+import com.roomwise.Models.Room;
 import com.roomwise.ObservePayments.Observer;
 import com.roomwise.Repositories.ReservationDAO;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -16,20 +18,21 @@ public class ReservationService implements Observer  {
     private PaymentService paymentService;
     private boolean paymentStatus;
     private Integer paymentId;
+    private RoomService roomService;
 
 
 
-    public ReservationService(ReservationDAO reservationRepository, Reservation reservation,PaymentService paymentService) {
+    public ReservationService(ReservationDAO reservationRepository, Reservation reservation,PaymentService paymentService, RoomService roomService) {
         this.reservationRepository = reservationRepository;
         this.paymentService = paymentService;
         this.paymentService.addObserver(this);
+        this.roomService = roomService;
     }
     public Integer getPaymentId() {
         return this.paymentId != null ? this.paymentId : 0; // Return a default value if paymentId is null
     }
     public void saveReservation(Reservation reservation) {
         reservation.setReservationId(IdService.getNextReservationID());
-        reservation.setCustomerId(IdService.getNextCustomerID());
         reservation.setPaymentId(IdService.getNextPaymentID());
         this.paymentId = reservation.getPaymentId();
         this.reservation = reservation;
@@ -58,6 +61,17 @@ public class ReservationService implements Observer  {
     public void updatePaymentStatus(boolean status ) {
         reservation.setPaymentStatus(status);
         // Update payment status
+    }
+
+    public BigDecimal getCharge(Reservation reservation) {
+        BigDecimal charge = new BigDecimal(0);
+        for (Integer number : reservation.getRoomsNumber()) {
+            Room room = roomService.getRoom(number);
+            if (room != null) {
+                charge.add(room.getCharge());
+            }
+        }
+        return charge;
     }
 
 
